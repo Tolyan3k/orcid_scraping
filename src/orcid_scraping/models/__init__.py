ead29c52e99e7e8dd1bed982c7baa96f05d661c4
Anatoliy Lazovyj <Lazoviy3000@ya.ru>
2025-06-22 17:55:22 +0300

    feat: ORCID type

---
+++ src/orcid_scraping/models/__init__.py
@@ -0,0 +1,10 @@
+from .orcid import Orcid
+from .researcher.work import OrcidResearcherWork
+from .scrape_result import ScrapeResult
+
+
+__all__ = [
+    "Orcid",
+    "OrcidResearcherWork",
+    "ScrapeResult",
+]
